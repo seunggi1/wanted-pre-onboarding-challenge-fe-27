4ce3c4af62a5037fd88f8e8bd1b7b67efa523fbc
kimsg <ksg9396@gmail.com>
2024-11-08 16:17:27 +0900

    라우터 코드 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import AuthForm from './components/AuthForm.tsx';
-import TodoList from './components/TodoList.tsx';
-import TodoDetail from './components/TodoDetail.tsx';
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		children: [
-			{
-				path: '/',
-				children: [
-					{
-						index: true,
-						element: <TodoList />,
-					},
-					{
-						path: '/todo/:id',
-						element: <TodoDetail />,
-					},
-				],
-			},
-			{
-				path: 'auth',
-				children: [
-					{
-						path: 'signup',
-						element: <AuthForm />,
-					},
-					{
-						path: 'signin',
-						element: <AuthForm />,
-					},
-				],
-			},
-		],
-	},
-]);
+import Router from './routers/router.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<Router />
 	</StrictMode>
 );
